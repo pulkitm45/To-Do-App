f6b05f1aaedba8384b5f89d28aad59ddb842f60f
pulkitm45 <pulkitmahajan5@gmail.com>
2024-03-22 11:32:18 +0530

    CheckBox

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -1,71 +1,83 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Box from '@mui/material/Box';
 import "./ComponentStyles/Heading.css"
 import Button from '@mui/material/Button';
 import Task from './Task';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 function Heading() {
 
     const [list, setlist] = useState([])
-    const [listItem,setlistItem] = useState("");
-    const [totalTasks,settotalTasks] = useState(0);
-    const [completedTasks,setcompletedTasks] = useState(0);
-    
+    const [listItem, setlistItem] = useState("");
+    const [totalTasks, settotalTasks] = useState(0);
+    const [completedTasks, setcompletedTasks] = useState(0);
+
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []);
+
 
     useEffect(() => {
         settotalTasks(list.length);
     }, [list])
-    
 
-    function handleChange(e){
+
+    function handleChange(e) {
         let val = e.target.value;
         setlistItem(val);
     }
-    
-    function handleAdd(){
-        setlist([...list,listItem])
+
+    function handleAdd() {
+        setlist([...list, listItem])
         setlistItem("")
     }
     return (
         <>
-        <div style={{display:'flex', flexDirection:'column', alignItems:"center", justifyContent:"center", color:"white", gap:"12px"}}>
-            <Box
-                height={100}
-                margin="auto"
-                borderRadius="40px"
-                width={250}
-                my={4}
-                display="flex"
-                alignItems="center"
-                gap={4}
-                p={2}
-                sx={{ border: '2px solid white' }}><div style={{display:'flex', flexDirection: 'column'}}><div><strong>To Do Done</strong></div>
-                <div>Keep it UP!</div></div><Box
-                height={55}
-                margin="auto"
-                borderRadius="50px"
-                width={60}
-                position="relative"
-                gap={4}
-                p={2}
-                my={4}
-                display="flex"
-                alignItems="center"
-                left={17}
-                backgroundColor="red"
-                sx={{ border: '2px solid red' }}
-                ><span style={{fontSize:"35px", color:"black"}}><strong>{completedTasks +"/"+totalTasks}</strong></span></Box></Box>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: "center", justifyContent: "center", color: "white", gap: "12px", border: "2px solid white", width: "40%", marginTop: "5px", margin: "auto" }}>
+                <h1>Plan your day!</h1>
+                <Box
+                    height={100}
+                    margin="auto"
+                    borderRadius="40px"
+                    width={250}
+                    my={4}
+                    display="flex"
+                    alignItems="center"
+                    gap={4}
+                    p={2}
+                    sx={{ border: '2px solid white' }}><div style={{ display: 'flex', flexDirection: 'column' }}><div><strong>To Do Done</strong></div>
+                        <div>Keep it UP!</div></div><Box
+                            height={55}
+                            margin="auto"
+                            borderRadius="50px"
+                            width={60}
+                            position="relative"
+                            gap={4}
+                            p={2}
+                            my={4}
+                            display="flex"
+                            alignItems="center"
+                            left={17}
+                            backgroundColor="red"
+                            sx={{ border: '2px solid red' }}
+                        ><span style={{ fontSize: "35px", color: "black" }}><strong>{completedTasks + "/" + totalTasks}</strong></span></Box></Box>
 
-                <div id='add' style={{display:'flex', width:"250px", alignItems:"center", justifyContent:"center", margin:"auto", gap:"30px"} }>
-                <input value={listItem} style={{width: "250px",height:"30px", margin: "auto", borderRadius:"10px", backgroundColor:"#232020", paddingLeft:"10px", color:"white"}} onChange={handleChange} placeholder='Write your next task!' type='text'/> <Button variant="contained" size="small" onClick={handleAdd}>Add</Button>
+                <div id='add' style={{ display: 'flex', width: "250px", alignItems: "center", justifyContent: "center", margin: "auto", gap: "30px" }}>
+                    <input ref={inputRef} value={listItem} style={{ width: "250px", height: "30px", margin: "auto", borderRadius: "10px", backgroundColor: "#232020", paddingLeft: "10px", color: "white" }} onChange={handleChange} placeholder='Write your next task!' type='text' />
+                    <Fab color="error" aria-label="add">
+                        <AddIcon onClick={handleAdd} />
+                    </Fab>
                 </div>
-                {list.length>0?
-                list?.map((item)=>{
-                    return(
-                        <Task key={item} item = {item}/>
-                    )
-                }):<h3 style={{marginTop:"20px"}}>No Tasks to Display!!</h3>}
-                </div>
+                {list.length > 0 ?
+                    list?.map((item) => {
+                        return (
+                            <Task key={item} item={item} completedTasks={completedTasks} setcompletedTasks={setcompletedTasks} />
+                        )
+                    }) : <h3 style={{ marginTop: "20px" }}>No Tasks to Display!!</h3>}
+            </div>
         </>
     )
 }
--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,25 +1,50 @@
-import React ,{useState} from 'react'
+import React, { useState } from 'react'
+import Fab from '@mui/material/Fab';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
 
 function Task(props) {
 
-    const [isChecked,setisChecked] = useState(false)
+    const [isChecked, setisChecked] = useState(false)
+    // const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 
-    function handleChange(){
+    function handleChange() {
         setisChecked(!isChecked)
+        if (isChecked) {
+            props.setcompletedTasks(props.completedTasks - 1)
+        }
+        else {
+            props.setcompletedTasks(props.completedTasks + 1)
+        }
     }
 
 
     return (
         <>
-            <div style={{
-                height: "28px", width: "267px", backgroundColor: "rgb(35, 32, 32)", margin: "10px", paddingLeft: "10px", borderRadius: "4px", paddingTop: "10px", display: "flex", justifyContent: "space-between", alignContent: "center"
+            <div 
+             style={{
+                height: "60px", width: "267px", backgroundColor: "rgb(35, 32, 32)", margin: "10px", paddingLeft: "10px", borderRadius: "4px", display: "flex", justifyContent: "space-between", alignItems: "center", overflow:"hidden"
             }}>
 
 
-                <input type='checkbox' onChange={handleChange} checked={isChecked} style={{ marginRight: "10px" }} />{props.item}
+                {/* <input type='checkbox' onChange={handleChange} checked={isChecked} style={{ marginRight: "10px" }} /><span style={{ textDecoration: isChecked ? 'line-through' : 'none', fontSize: "20px" }}>{props.item}</span> */}
+                <Checkbox size="small" onChange={handleChange} checked={isChecked} style={{ marginRight: "10px", color:"white" }} /><span style={{ textDecoration: isChecked ? 'line-through' : 'none', fontSize: "20px" }}>{props.item}</span> 
                 <div style={{ flexGrow: "1" }}></div>
-                <button style={{ marginRight: "10px", width: "60px", marginBottom: "5px" }}>Edit</button><button style={{ marginRight: "10px", width: "60px", marginBottom: "5px" }}>Delete</button>
+                <Box sx={{ '& > :not(style)': { m: 1 } }}>
+                <Fab color="grey" aria-label="edit" size="small">
+                    <EditIcon/>
+                </Fab>
+                </Box>
+                {/* <button style={{ marginRight: "10px", width: "60px", marginBottom: "5px" }}>Edit</button> */}
+                <Box sx={{ '& > :not(style)': { m: 1 } }}>
+                <Fab color="grey" aria-label="delete" size="small">
+                    <DeleteIcon/>
+                </Fab>
+                </Box>
+                {/* <button style={{ marginRight: "10px", width: "60px", marginBottom: "5px" }}>Delete</button> */}
             </div>
         </>
     )
